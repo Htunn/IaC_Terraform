951434e73af3f6cb57237f788d990376993977a2
Htunn <martin.mystery9@gmail.com>
2020-12-06 20:14:12 +0630

    initial terraform commit

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "ap-southeast-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-0c20b8b385217763f"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "terraform-example"
+    }
+}
